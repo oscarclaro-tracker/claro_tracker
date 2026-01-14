673d23cdc5d86ff8f31ac2a301e4321a3f161776
claroads <clarocolombiaads@gmail.com>
2026-01-14 14:09:30 -0500

    debug_mode_false

--- tracking/views.py
+++ tracking/views.py
@@ -179,7 +179,6 @@ def collect_event(request):
         params.update({
             "page_location": event.path,
             "engagement_time_msec": 1,
-            "debug_mode": True
         })
 
         # 5️⃣ Enviar a GA4
